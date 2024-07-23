5460aaebc9d63b0ac15ef3c1eed405d7dc9186fc
lowkieman <lowkieman@gmail.com>
2024-07-23 16:00:25 +0300

    first js code

---
+++ js/header.js
@@ -0,0 +1,50 @@
+document.getElementById("profile1").style.display="none";
+
+function profile1(){
+    document.getElementById("profile1").style.display="block";
+    document.getElementById("profile1").style.position="absolute";
+    document.getElementById("profile1").style.top="100px";
+    document.getElementById("profile1").style.right="0";
+}
+//onclick function
+function background1(){
+    document.getElementById("background1").style.borderBottom="2px solid blue"
+}
+function background2(){
+    document.getElementById("background2").style.borderBottom="2px solid blue";
+}
+function background3(){
+    document.getElementById("background3").style.borderBottom="2px solid blue"
+
+}
+function background4(){
+    document.getElementById("background4").style.borderBottom="2px solid blue"
+
+}
+function background5(){
+    document.getElementById("background5").style.borderBottom="2px solid blue"
+
+}
+function background6(){
+    document.getElementById("background6").style.borderBottom="2px solid blue";
+    
+}
+//on mouse leave function
+function background11(){
+    document.getElementById("background1").style.borderBottom="2px solid blue";    
+}
+function background22(){
+    document.getElementById("background2").style.borderBottom="2px solid blue";    
+}
+function background33(){
+    document.getElementById("background3").style.borderBottom="2px solid blue";    
+}
+function background44(){
+    document.getElementById("background4").style.borderBottom="2px solid blue";    
+}
+function background55(){
+    document.getElementById("background5").style.borderBottom="2px solid blue";    
+}
+function background66(){
+    document.getElementById("background6").style.borderBottom="2px solid blue";    
+}
