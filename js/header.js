05196f140943a5f856900561738e36c46dcb8bd3
lowkieman <lowkieman@gmail.com>
2024-07-28 18:19:01 +0300

    header js

--- js/header.js
+++ js/header.js
@@ -2,4 +2,4 @@ const accountAction=document.getElementById("getaccount-action");
 
 accountAction.addEventListener("click",function(){
     document.getElementById("account-action").style.display="block";    
-})
+});
