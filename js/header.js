a4befcf80ff3c51fc934353dcdda3a0a6206ad40
lowkieman <lowkieman@gmail.com>
2024-08-19 14:00:14 +0300

    improved header js

--- js/header.js
+++ js/header.js
@@ -95,22 +95,6 @@ background4.addEventListener("click",function(){
     }
 });
 
-//LEGACY POINTS PAGE *REDEEM
-background5.addEventListener("click",function(){    
-    document.getElementById("nav").style.left="-50%";
-    document.getElementById("hideNav").style.visibility="collapse";
-    document.getElementById("alx").style.display="none";//alx page
-    document.getElementById("community").style.display="none"//community page
-    document.getElementById("opportunities").style.display="none";//opportunities page
-    document.getElementById("profile").style.display="none";//profile page
-    document.getElementById("redeem").style.display="block";//legacy points page *redeem
-    document.getElementById("contact-us").style.display="none";//contact us page
-    document.getElementById("login").style.display="none";//login
-    if(window.innerWidth<=425){
-        document.getElementById("showNav").style.visibility="visible";
-    }
-});
-
 //CONTACT US PAGE
 background6.addEventListener("click",function(){
     document.getElementById("nav").style.left="-50%";
