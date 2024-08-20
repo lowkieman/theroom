58e1f2d190d8152cd2236b854f3b37c35b5e9402
lowkieman <lowkieman@gmail.com>
2024-08-20 14:14:51 +0300

    added js for year in the footer

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 //Behavior of main elements
 //#alx,#community,#opportunities,#profile,#redeem,#contact-us,#login
+const getYear=new Date().getFullYear();
+
+document.getElementById("getYear").innerHTML=getYear;
