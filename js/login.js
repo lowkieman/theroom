1098591ce9e3292a045fd6ac847586877a47ba73
lowkieman <lowkieman@gmail.com>
2024-09-13 22:46:26 +0300

    update login elem

--- js/login.js
+++ js/login.js
@@ -16,4 +16,13 @@ window.addEventListener("load",function(){
     document.getElementById("footer").style.position="fixed";
     document.getElementById("footer").style.bottom="0";
     document.getElementById("footer").style.height="10vh";
-})
+});
+
+const submit=document.getElementById("submit");
+submit.addEventListener("click",function(){
+    document.getElementById("alx").style.display="block";
+    document.getElementById("login").style.display="none"; 
+    document.getElementById("nav").style.display="block";
+    document.getElementById("account").style.visibility="visible";
+    document.getElementById("header").style.backgroundColor="rgb(242, 244, 247)";
+    document.getElementById("footer").style.position="static";})
